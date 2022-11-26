a0a34c20a0f6bb5962f5874c17b8f2ad74211b53
Abhilash <abhilashjha335@gmail.com>
2022-11-27 00:53:48 +0530

    Creating an endpoint for create a new user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,19 +8,35 @@ router.post('/createuser',
 [body('name', 'Enter a valid name').isLength({ min: 3 }),
 body('email', 'Enter a valid email').isEmail(),
 body('password', 'Password must be contained atleast 5 characters.').isLength({ min: 5 })],
-(req, res)=>{
+async(req, res)=>{
+
+    //If there are errors, return Bad request and the errors.
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     
-    //it will save the data in mongodb users database.
-    Users.create({
+    //Check whether the user with this email exists already or not
+   try{ 
+    
+    let user = await Users.findOne({email : req.body.email});
+    if(user){
+        return res.status(400).json({error: "Sorry a user with this email already exists."})
+    }
+
+    //Create a user and it will save the data in mongodb users database.
+  user =  await Users.create({
         name: req.body.name,
         password: req.body.password,
         email: req.body.email,
-      }).then(user => res.json(user))
-      .catch(err => console.log(err));   
+      })
+      
+    res.json(user);
+   }
+   catch(error){
+    console.error(error.message);
+    res.status(500).send("Some error occured!");
+   }
 })
 
 module.exports = router;
