0fe680f4e27eb0e58739f8193c93808cc41c6dcb
Abhilash <abhilashjha335@gmail.com>
2022-11-27 21:32:32 +0530

    Using hash fn and salt to secure user's pswrd And generating authToken

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const Users = require('../models/Users');
 const { body, validationResult } = require('express-validator');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+
+const JWT_SECRET = '@bhilash$1221$@'
 
 // Create a user using: POST '/api/auth/createuser'. No Login required
 router.post('/createuser',
@@ -16,22 +20,34 @@ async(req, res)=>{
       return res.status(400).json({ errors: errors.array() });
     }
     
-    //Check whether the user with this email exists already or not
-   try{ 
-    
+    try{ 
+      
+      //Check whether the user with this email exists already or not
     let user = await Users.findOne({email : req.body.email});
     if(user){
         return res.status(400).json({error: "Sorry a user with this email already exists."})
     }
+    
+    //Using hash function and salt for secure the password.
+    const salt = await bcrypt.genSalt(10);
+    const securePassword = await bcrypt.hash(req.body.password, salt);
 
     //Create a user and it will save the data in mongodb users database.
   user =  await Users.create({
         name: req.body.name,
-        password: req.body.password,
+        password: securePassword,
         email: req.body.email,
       })
       
-    res.json(user);
+    const data = {
+      user:{
+        id: user.id  
+      }
+     } 
+
+   const authToken = jwt.sign(data, JWT_SECRET);
+    // res.json(user);
+    res.json({authToken});
    }
    catch(error){
     console.error(error.message);
