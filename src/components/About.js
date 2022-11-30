272c980e962688e63e4726b7fadaae32f3a13ad2
Abhilash <abhilashjha335@gmail.com>
2022-11-30 20:17:14 +0530

    Setup for notebook webapp

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      This is about dude!
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      this is home dude!
+    </div>
+  )
+}
+
+export default Home
