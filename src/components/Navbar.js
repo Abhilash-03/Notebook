619ee65d08ba6e76c0e0f96d305ee1a7375e7778
Abhilash <abhilashjha335@gmail.com>
2023-01-21 21:34:05 +0530

    Add icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">Notebook</Link>
+    <Link className="navbar-brand img" to="/"><img src="favicon-32x32.png" alt="" /></Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
